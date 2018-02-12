39e76eadf311aa0f822693dfa07bfdd224a5c7bf
poseidondev <sh.an.ebuckley92@gmail.com>
2018-02-12 02:28:30 +0000

    fix masternode collateral value

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -12,7 +12,7 @@
 #include "spork.h"
 
 //
-// Bootup the Masternode, look for a 25000 POSQ input and register on the network
+// Bootup the Masternode, look for a 10000 POSQ input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
