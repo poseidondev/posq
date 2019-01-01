09abd89fad3c580cca1c60975113046829e370f8
sicXnull <cvanhauterjr@gmail.com>
2018-12-31 18:54:08 -0800

    Pixmaps, Icon Changes & Revert to 1hr nStakeMinAge for testing

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ bool fVerifyingBlocks = false;
 unsigned int nCoinCacheSize = 5000;
 bool fAlerts = DEFAULT_ALERTS;
 
-unsigned int nStakeMinAge = 3 * 60 * 60; // 3 Hours
+unsigned int nStakeMinAge = 60 * 60; // 1 Hour for testing
 int64_t nReserveBalance = 0;
 
 /** Fees smaller than this (in duffs) are considered zero fee (for relaying and mining)
